cb063f5ebd1eb9760769151e2433aa961fc5e69a
Jose felipe <josefelipesilvasantos874@gmail.com>
2022-03-25 20:32:22 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string nome, sobrenome, sobrenomeEmMaiusculas; 
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiusculas = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de catalogo: {sobrenomeEmMaiusculas}, {nome}"); 
+
+
